207ef27356488cde6a90204a1c580d20e342a4c8
songyuchencyan <51196320+songyuchencyan@users.noreply.github.com>
2020-09-25 19:36:27 +0800

    Update Extraction_lexique_morphosyntaxique.py

--- Scripts/Extraction_lexique_morphosyntaxique.py
+++ Scripts/Extraction_lexique_morphosyntaxique.py
@@ -13,8 +13,8 @@ time_start=time.time()
 # 读取该目录里所有文件
 liste_noms_fichiers = ["80_corpus_concat.conllu"] # Insérez les noms des fichiers CONLL-U dans cette liste
 folder_path = "." # Insérez le chemin absolu qui contient des fichiers CONLL-U de la liste "liste_noms_fichiers"
-# for files in file_list:
-    # fichier_conll = open(folder_path + "/" + files,"r", encoding="UTF-8")
+# for files in file_list: # Parcourir tous les fichiers CONLL-U de la liste
+    # fichier_conll = open(folder_path + "/" + files,"r", encoding="UTF-8") # Lire chaque fichier CONLL-U de la liste
 for fichier in liste_noms_fichiers: # Parcourir tous les fichiers CONLL-U de la liste
     fichier_conll = open(folder_path + "/" + fichier,"r", encoding="UTF-8") # Lire chaque fichier CONLL-U de la liste
     # Lire le fichier conll ligne par ligne
