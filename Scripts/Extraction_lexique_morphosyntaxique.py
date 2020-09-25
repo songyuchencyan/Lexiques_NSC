e33f12ed64a00369f031335e3555056028efcc6e
songyuchencyan <51196320+songyuchencyan@users.noreply.github.com>
2020-09-25 19:34:09 +0800

    Update Extraction_lexique_morphosyntaxique.py

--- Scripts/Extraction_lexique_morphosyntaxique.py
+++ Scripts/Extraction_lexique_morphosyntaxique.py
@@ -8,15 +8,15 @@ time_start=time.time()
 ##### 提取完拿excel去一遍重，把大小写不一样的去了，Basic和basic。 #####
 
 ##### 读取目录里的文件 Lire tous les fichiers du répertoire #####
-# folder_path = "E:/TAL/Stage/arborator/projects/Scripts/test"  # 目标文件目录
-# file_list = os.listdir(folder_path)
+# folder_path = "E:/TAL/Stage/arborator/projects/Scripts/test"  # Insérez le chemin absolu qui contient des fichiers CONLL-U 
+# file_list = os.listdir(folder_path) # Liste qui contient les noms des fichiers CONLL-U
 # 读取该目录里所有文件
-liste_noms_fichiers = ["80_corpus_concat.conllu"]
-folder_path = "."
+liste_noms_fichiers = ["80_corpus_concat.conllu"] # Insérez les noms des fichiers CONLL-U dans cette liste
+folder_path = "." # Insérez le chemin absolu qui contient des fichiers CONLL-U de la liste "liste_noms_fichiers"
 # for files in file_list:
     # fichier_conll = open(folder_path + "/" + files,"r", encoding="UTF-8")
-for fichier in liste_noms_fichiers:
-    fichier_conll = open(folder_path + "/" + fichier,"r", encoding="UTF-8")
+for fichier in liste_noms_fichiers: # Parcourir tous les fichiers CONLL-U de la liste
+    fichier_conll = open(folder_path + "/" + fichier,"r", encoding="UTF-8") # Lire chaque fichier CONLL-U de la liste
     # Lire le fichier conll ligne par ligne
     for ligne in fichier_conll:
         if ligne[0]!="#" and ligne!="\n" and ligne!="":
@@ -27,8 +27,8 @@ for fichier in liste_noms_fichiers:
             trait = ligne[5].replace(' ','')
             glose = re.sub('AlignBegin.+Gloss=', '', ligne[9]).replace(' ','').replace('\n','')
             terme = mot + "\t" + cat + "\t" + trait + "\t" + lemme + "\t" + glose + "\n"
-            lexique = open("Lexique.txt","a", encoding="UTF-8")
-            voir_lexique = open("Lexique.txt","r", encoding="UTF-8")
+            lexique = open("Lexique.txt","a", encoding="UTF-8") # Insérez le chemin absolu du fichier de lexique morphosyntaxique
+            voir_lexique = open("Lexique.txt","r", encoding="UTF-8") # Insérez le chemin absolu du fichier de lexique morphosyntaxique, cette étape vise à vérifier si c'est une nouvelle entrée
             contenu = voir_lexique.readlines()    
             if terme not in contenu:
                 lexique.write(terme)
